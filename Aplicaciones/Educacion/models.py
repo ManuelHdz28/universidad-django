387c7d8024dc6b36f8ad11a983f3106dc710160d
manuel <manuel.hernandez28@outlook.com>
2025-06-02 19:38:42 -0600

    feat: agregar modelo Carrera y migración inicial

--- Aplicaciones/Educacion/models.py
+++ Aplicaciones/Educacion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Carrera(models.Model):
+    id_car = models.AutoField(primary_key=True)
+    nombre_car = models.CharField(max_length=100)
+    fecha_creacion_car = models.DateTimeField(auto_now_add=True)
+    telefono_car = models.CharField(max_length=9, blank=True, null=True)
+    
