2e964a4f075a0aabc6f984ceec5ee6bdb389823e
manuel <manuel.hernandez28@outlook.com>
2025-06-05 17:17:45 -0600

    feat: implementar funcionalidades de edición y eliminación de carreras

--- Aplicaciones/Educacion/urls.py
+++ Aplicaciones/Educacion/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('carreras/', views.carreras, name='carreras'),
     path('crear_carrera/', views.crear_carrera, name='crear_carrera'),
+    path('eliminar_carrera/<id>', views.eliminar_carrera, name='eliminar_carrera'),
+    path('editar_carrera/<id>', views.editar_carrera, name='editar_carrera'),
 ]
 
--- Aplicaciones/Educacion/views.py
+++ Aplicaciones/Educacion/views.py
@@ -26,6 +26,30 @@ def crear_carrera(request):
         )
         
         return redirect('/carreras')
+    
+def eliminar_carrera(request, id):
+    try:
+        carreradb = Carrera.objects.get(id_car=id)
+        carreradb.delete()
+        return redirect('/carreras')
+    except Carrera.DoesNotExist:
+        return render(request, 'error.html', {'message': 'Carrera no encontrada.'})
+    
+def editar_carrera(request, id):
+    try:
+        carreradb = Carrera.objects.get(id_car=id)
+        
+        if request.method == 'POST':
+            carreradb.nombre_car = request.POST.get('nombre_car')
+            carreradb.fecha_creacion_car = request.POST.get('fecha_creacion_car')
+            carreradb.telefono_car = request.POST.get('telefono_car')
+            carreradb.save()
+            return redirect('/carreras')
+        
+        return render(request, 'editar_carrera.html', {'carrera': carreradb})
+    
+    except Carrera.DoesNotExist:
+        return render(request, 'error.html', {'message': 'Carrera no encontrada.'})
         
         
     
